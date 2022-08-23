1c07a1d1dd9395ecc4995396859a3ea91e6d2719
Jonghyun <starrything@gmail.com>
2022-08-23 17:47:34 +0900

    update some codes...

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Jonghyun]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Jonghyun'
+export const siteTitle = 'Sample Website'
 
 export default function Layout({
   children,
